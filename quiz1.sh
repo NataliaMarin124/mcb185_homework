83c1f521f1e8e1495e410030dcb7701979823407
NataliaMarin124 <natmarin@ucdavis.edu>
2024-01-23 09:22:11 -0800

    up

---
+++ quiz1.sh
@@ -0,0 +1,5 @@
+Natalia Marin zsh 
+gunzip -c dictionary | grep "a" | grep -v "[^aumcfto]" 
+gunzip -c dictionary | grep "b" | grep -v "[^atrnlib]" 
+gunzip -c dictionary | grep "c" | grep -v "[^camdino]"
+gunzip -c dictionary | grep "z" | grep -v "[^znaigro]"
