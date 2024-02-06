0d906eed6e72c654ef2914869ad39e86fe5e5900
NataliaMarin124 <natmarin@ucdavis.edu>
2024-02-06 09:22:18 -0800

    update

--- 38quiz.py
+++ 38quiz.py
@@ -1,1 +1,11 @@
 # 38quiz.py by Natalia Marin and Roger Xu and Yutong Ji 
+
+pi = 3
+sign = -1
+for n in range(1, 30, 2):
+	pi = 4 *(1 + sign*(1/(n+2)))
+	print(n, sign, pi)
+	
+
+	sign = -sign  
+	
