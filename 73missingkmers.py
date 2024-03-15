659578670f889993806a0502d2e7c92f2a34f17e
NataliaMarin124 <natmarin@ucdavis.edu>
2024-03-14 20:26:49 -0700

    update

--- 73missingkmers.py
+++ 73missingkmers.py
@@ -15,11 +15,9 @@ for k in range(4, 10):
 	kcount= {}
 	for defline, seq in mcb185.read_fasta(fasta):
 		rev_seq = dogma.reversecomp(seq)
-		print(seq[:30])
-		print(rev_seq[:30])
 
 		for i in range(len(rev_seq) -k +1):
-			kmer = seq[i:i+k]
+			kmer = rev_seq[i:i+k]
 			if kmer not in kcount: kcount[kmer] = 0
 			kcount[kmer] += 1
 			#print(kmer)
